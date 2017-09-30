92556dd85b11b78e31e0da1236c7c6f50e58c118
artemstukan <artem.stukan@gmail.com>
2017-09-30 18:31:52 +0300

    #19 - Search Bar value is now controlled by state

---
+++ src/components/search_bar.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+	constructor(props) {
+		super(props); // ask Simon-Alexy about super
+
+		this.state = { term: '' };
+	}
+
+	render() {
+		// return <input onChange={this.onInputChange} onClick={this.onInputClick} />;
+		return (
+			<div>
+				<input 
+					value = {this.state.term}
+					onChange={event => this.setState({ term: event.target.value })} />;
+			</div>
+		)
+	}
+
+	// onInputChange(event) { //Why it has event argument here but not in onInputChange?
+	// 	console.log("changed!");
+	// 	console.log(event); //Why target field is null in Chrome Console?
+	// 	console.log(event.target); //Why here it is an HTML element?
+	// 	console.log(event.target.value); //Why is it suddently a real value we typed in???
+	// }
+
+	onInputClick(event) {
+		console.log("clicked!");
+		console.log(event);
+	}
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyATQwkMI-DSD1ROaxCPampE8tvPSILsu1k';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Create new component. This component should produce some HTML
+const App = () => {
+	return (
+		<div>
+			<SearchBar />
+		</div>
+	);
+}
+
+// Take this component's generated HTML and put it on the page (in the DOM)
+
+ReactDOM.render(<App />, document.querySelector('.container')); // Why do we do it like this?
+
+/* And not like this? 15! out of 149
+var myApp = new App();
+ReactDOM.render(myApp, document.querySelector('.container'));
+*/
